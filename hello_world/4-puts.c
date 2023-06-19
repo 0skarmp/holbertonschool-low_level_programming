31ab68e7718e4ac7cf4d8c0bc391f6b86759e9f5
0skarmp <omoralespj@gmail.com>
2023-06-19 12:31:44 -0700

    programming in 'C' using 'puts'

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+ * main - firts words
+ *
+ * Description: print on the screen
+ *
+ * Return: sucess
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
