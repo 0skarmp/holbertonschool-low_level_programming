e1db5f02da42ab7c52bd6f2b94caaf304e48042c
0skarmp <omoralespj@gmail.com>
2023-06-19 14:59:01 -0700

    size 'oskar'

---
+++ hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - last words
+ *
+ * Description: write on the screen
+ *
+ * Return: zero on success
+ */
+int main(void)
+{
+	int o;
+	long int s;
+	long long int k;
+	char a;
+	float r;
+
+	printf("Size of a int: %lu byte(s)\n", (s)sizeof(o));
+	printf("Size of a long int: %lu byte(s)\n", (s)sizeof(s));
+	printf("Size of a long long int: %lu byte(s)\n", (s)sizeof(k));
+	printf("Size of a char: %lu byte(s)\n", (s)sizeof(a));
+	printf("Size of a float: %lu byte(s)\n", (s)sizeof(r));
+	return (0);
+}
+
