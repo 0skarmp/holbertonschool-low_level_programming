044412654d9a72ceba2634fdcf34a5c2f3e9a1d8
0skarmp <omoralespj@gmail.com>
2023-06-20 16:07:08 -0700

    printing numbers with putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - characters upper and lower case
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+int oskar
+for(oskar = '0'; oskar <= '10'; oskar++)
+putchar(oskar);
+putchar('\n');
+return (0);
+}
