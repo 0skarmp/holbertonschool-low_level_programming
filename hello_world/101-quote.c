8e0f37912e51a3e128ad7e506b6e804550004853
0skarmp <omoralespj@gmail.com>
2023-06-19 21:05:25 -0700

    print exactlytext with standar error

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,15 +1,16 @@
-#include <studio.h>
-
+#include <unistd.h>
+#include <string.h>
 /**
  * main - first 101
  *
- * Description: print
+ * Description: write a C program to print specific text with standar error
  *
  * Return: one on succes
  */
 int main(void)
 {
-char a[] = "and that piece of art is useful\" - Dora Korpar, 2015^Z-10-19\n";
-write(STDERR_FILENO, a, sterlen(a));
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015^Z-10-19\n";
+int len = strlen(msg);
+write(STDERR_FILENO, msg, len);
 return (1);
 }
