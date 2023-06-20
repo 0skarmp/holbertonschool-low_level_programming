179409dd42d773162753aefcb7b89969494bad90
0skarmp <omoralespj@gmail.com>
2023-06-20 15:53:15 -0700

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -print numbers
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+int oskar;
+for (oskar = 0; oskar < 10; oskar++)
+printf("%d", oskar);
+printf("\n");
+return (0);
+}
