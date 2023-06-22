ec176aac3b524f40a77570c478de4ae8fb0eca86
0skarmp <omoralespj@gmail.com>
2023-06-22 13:39:33 -0700

    printing 10times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+*followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+int time = 0;
+char oskar = "a";
+while (time < 10)
+{
+while (oskar <= "z")
+{
+_putchar(oskar);
+oskar++;
+}
+putchar ('\n');
+time++
+}
+}
