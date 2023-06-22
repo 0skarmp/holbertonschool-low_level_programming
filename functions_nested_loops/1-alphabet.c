7f4d8a4fd66acd14cf21e224f2d851f5c6ac53b6
0skarmp <omoralespj@gmail.com>
2023-06-21 22:49:38 -0700

    print lowecase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+void print_alphabet(void)
+{
+char oskar = "a";
+
+for (oskar = a; oskar <= z; oskarii)
+print_alphatbet(oskar);
+print ('/n')
+}
