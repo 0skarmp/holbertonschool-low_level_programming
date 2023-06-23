d07f285009e8f64a5a119cf9bd6fba93d04e2a69
0skarmp <omoralespj@gmail.com>
2023-06-23 15:22:34 -0700

    print sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,27 +1,31 @@
 #include"main.h"
 
 /**
- * print_sign - Check HOLBERTON
+ * print_sign - Check Holberton
  * @n: An input number
- * Description: This function prints the sign of the number
+ * Description: Tohis function prints the sign of the number
  * Return: 1 if number is positive, o if number is 0, or
  * -1 if number is negative
  */
-int print(int n)
+int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else
-{
-_putchar('-');
-return (-1);
-}
+	int retorno;
+
+	if (n > 0)
+	{
+		retorno = (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		retorno = (0);
+		_putchar('0');
+	}
+	else
+	{
+		retorno = (-1);
+		_putchar('-');
+	}
+
+	return (retorno);
 }
