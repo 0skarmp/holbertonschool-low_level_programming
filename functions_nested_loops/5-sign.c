b7c7b17d95f4745012ae09c1d57a9b59361ab6b3
0skarmp <omoralespj@gmail.com>
2023-06-23 15:11:33 -0700

    print sign and alpha

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,25 +1,27 @@
 #include"main.h"
 
 /**
-* print_sign - function to check for sign and number,
-*
-* Return: Always 0 (Success)
-*/
-int print_sign(int n)
+ * print_sign - Check HOLBERTON
+ * @n: An input number
+ * Description: This function prints the sign of the number
+ * Return: 1 if number is positive, o if number is 0, or
+ * -1 if number is negative
+ */
+int main(void)
 {
-	if (n > 0)
-	{
-	_putchar('+');
-	return (0);
-	}
-	else if (n == 0)
-	{
-	_putchar('0');
-	return (0);
-	}
-	else
-	{
-	_putchar('-');
-	return (-1);
-	}
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
