1b9f9ef4a7e9fa67494ce181ebc06c7282319cb8
0skarmp <omoralespj@gmail.com>
2023-06-24 23:48:37 -0700

    24 hours

--- functions_nested_loops/8-main.c
+++ functions_nested_loops/8-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-11;rgb:0000/0000/0000/**
-		      * main - check the code
-		      *
-		      * Return: Always 0.
-		      */
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
   jack_bauer();
