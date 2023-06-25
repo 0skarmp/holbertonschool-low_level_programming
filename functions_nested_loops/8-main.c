f6afb9382acce5004546da53b7b30015dd432149
0skarmp <omoralespj@gmail.com>
2023-06-24 22:28:38 -0700

    learn your times table

---
+++ functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+11;rgb:0000/0000/0000/**
+		      * main - check the code
+		      *
+		      * Return: Always 0.
+		      */
+int main(void)
+{
+  jack_bauer();
+  return (0);
+}
