3240745da8bb39580672c43758944e7d581af5f4
0skarmp <omoralespj@gmail.com>
2023-06-26 11:33:38 -0700

    fuction to update value of variable

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *reset_to_98 - updates the value of the variable
+ * pointer points to  to 98
+ *@n: pointer to  the variable to update
+ *return: Alaways 0 (succes)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
