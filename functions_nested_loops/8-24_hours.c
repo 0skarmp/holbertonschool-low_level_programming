f006527601215cba32e19c5c9ce081ef54c529a1
0skarmp <omoralespj@gmail.com>
2023-06-25 19:45:21 -0700

    24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,9 +8,10 @@
 void jack_bauer(void)
 {
 	int H, M;
-for (H = 0; H <= 23; H++)
+
+	for (H = 0; H < 24; H++)
 	{
-		for (M = 0; M <= 59; M++)
+		for (M = 0; M < 60; M++)
 		{
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
