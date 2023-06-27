bf5355f4202f20b6c4214bb574a95a0fa37997b5
0skarmp <omoralespj@gmail.com>
2023-06-26 20:21:41 -0700

    function that prints a string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_rev -prints a string in reverse, followed by new line
+ * @s: string to be printing
+ *
+ *
+ */
+
+void print_rev(char *s)
+{
+
+int y, alm, i;
+
+	y = 0;
+
+	while (s[y] != 0)
+	{
+		y++
+	}
+
+	alm = y - 1;
+
+	for (i = 0; i <= alm; i++)
+	{
+		_putchar(s[alm - i]);
+	}
+
+	_putchar ('\n');
+}
