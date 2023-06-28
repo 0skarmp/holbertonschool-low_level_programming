700dd067602886c84b3f3812220e3e8746cfdb45
0skarmp <omoralespj@gmail.com>
2023-06-28 10:20:13 -0700

    printing

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * int main - print the name of the file
+ * @argc: argunment count
+ * @argv: argument vector
+ * main - firs printing
+ * Description : print in the screen
+ * Return: zero on success
+*/
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
