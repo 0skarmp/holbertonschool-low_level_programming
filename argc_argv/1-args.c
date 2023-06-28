bd474f7e44377872717bf9e99c4c4c542d043d94
0skarmp <omoralespj@gmail.com>
2023-06-28 14:11:10 -0700

    print number

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: argunment count
+ * @argv: argument vector
+ * main - firs printing
+ * Description : print in the screen
+ * Return: zero on success
+*/
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+
+       printf("%d\n", argc-1);
+    return (0);
+}
+
+
+
