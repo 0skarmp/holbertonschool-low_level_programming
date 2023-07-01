5db3cfbad19a4cff5a4d73ed480f21889a57cadf
0skarmp <omoralespj@gmail.com>
2023-07-01 11:04:17 -0700

    string to reserse

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * rev_string - reverse string "backwards"
+ * @s: string to be resersed
+ */
+
+void rev_string(char *s)
+{
+	int num, i, alm;
+	char tmp;
+
+	num = 0;
+
+	while (s[num] != 0)
+	{
+		num++;
+	}
+	alm = num - 1;
+	for (i = 0; i < num / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[num];
+	s[alm--] = tmp;
+	}
+}
