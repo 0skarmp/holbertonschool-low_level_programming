3c9fa6c89010f4c90785f6227509e3fd8869aee0
0skarmp <omoralespj@gmail.com>
2023-07-02 00:46:35 -0700

    print second half of numbers

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int x, n, i;
+
+	x = 0;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+
+	if (x % 2 == 0)
+	{
+		for (i = x / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (x % 2)
+	{
+		for (n = (x - 1) / 2; n < x - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
