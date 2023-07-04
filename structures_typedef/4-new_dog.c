b1123e03c893beb8fb1a9345f027f27e65869a0e
0skarmp <omoralespj@gmail.com>
2023-07-03 21:40:37 -0700

    fix errrors betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,47 +3,46 @@
 #include <string.h>
 #include <stdlib.h>
 /**
- */
+ * new_dog - to separate memory address
+ * @name: first parameter
+ * @age: second parameter
+ * @owner: third parameter
+ * Return: newdog
+  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int lenowner, lenname, i, j;
 
-  int lenowner, lenname, i, j;
+	dog_t *newdog;
 
-  dog_t *newdog;
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
+	return (NULL);
 
-  newdog = malloc(sizeof(dog_t));
-  if (newdog == NULL)
-    return (NULL);
+	lenowner = strlen(owner);
+	lenname = strlen(name);
 
-  lenowner = strlen(owner);
-  lenname = strlen(name);
+	newdog->name = malloc(lenname + 1);
+	newdog->owner = malloc(lenowner + 1);
 
-  newdog->name = malloc(lenname + 1);
-  newdog->owner = malloc(lenowner + 1);
+	if (newdog->name == NULL || newdog->owner == NULL)
+	{
+		free(newdog);
+		free(newdog->name);
+		free(newdog->owner);
 
-  if(newdog->name == NULL ||newdog->owner == NULL)
-    {
-      free(newdog);
-      free(newdog->name);
-      free(newdog->owner);
+		return (NULL);
+	}
+		for (i = 0; i < lenname + 1; i++)
+	{
+		newdog->name[i] = name[i];
+	}
+		newdog->age = age;
 
-      return (NULL);
-    }
-  for(i = 0;i < lenname +1; i++)
-    {
-      newdog->name[i] = name[i];
-    }
-  newdog->age = age;
+		for (j = 0; j < lenowner + 1; j++)
+	{
+		newdog->owner[j] = owner[j];
+	}
 
-  for(j = 0;j < lenowner +1; j++)
-     {
-      newdog->owner[j] = owner[j];
-    }
-
-  return (newdog);
+	return (newdog);
 }
-
-
-
-
- 
