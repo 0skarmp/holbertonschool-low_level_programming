c34171203ea4a09be99e1aeaa0ee2134d137d1c8
0skarmp <omoralespj@gmail.com>
2023-07-03 21:47:10 -0700

    fix errrors betty

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * free_dog - fuction that frees memory
+ * @d: first parameter
  */
 
 void free_dog(dog_t *d)
 {
-  if (d)
-    {
-    free(d->name);
-     free(d->owner);
-     free (d);
-    }
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
