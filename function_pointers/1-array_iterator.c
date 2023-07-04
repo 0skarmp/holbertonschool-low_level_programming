d95b2bd62f10fce38c4a694c2b5859d48a9a837c
0skarmp <omoralespj@gmail.com>
2023-07-04 11:25:50 -0700

    executes a function given as a parameters

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,12 +11,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  size_t i;
- 
-  if (array != NULL && action != NULL)
-    {  
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
 	for (i = 0; i < size; i++)
-		action(array[i]);
-    }	       
-	
+	action(array[i]);
+	}
+
 }
