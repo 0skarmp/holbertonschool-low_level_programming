ef4758f6bfd43677e4a85ce6fd6b60db8acd9bf9
0skarmp <omoralespj@gmail.com>
2023-07-04 11:21:21 -0700

    fix mistakes

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include <stddef.h>
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * array_iterator - counter the size
  * @array:first parameter
@@ -10,14 +10,13 @@
  *
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
-
 {
-	size_t i;
-
-
+  size_t i;
+ 
+  if (array != NULL && action != NULL)
+    {  
 	for (i = 0; i < size; i++)
-	{
 		action(array[i]);
-	       
-	}
+    }	       
+	
 }
